2b723a91d29cc3d0855f0d9649fb1e462764f343
ashish493 <malikashish493@gmail.com>
2023-10-01 10:35:06 +0530

    Exposed the Sail and Deck functions through API and integrated metrics using goprocinfo

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require github.com/docker/docker v24.0.6+incompatible
 
 require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/c9s/goprocinfo v0.0.0-20210130143923-c95fcf8c64a8 // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3 // indirect
 	github.com/google/uuid v1.3.1 // indirect
---
+++ sailor/handlers.go
@@ -0,0 +1,72 @@
+package sailor
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ashish493/ormen/sail"
+	chi "github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
+
+func (a *Api) GetStatsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(a.Worker.Stats)
+}
+
+func (a *Api) StartTaskHandler(w http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+
+	te := sail.SailEvent{}
+	err := d.Decode(&te)
+	if err != nil {
+		msg := fmt.Sprintf("Error unmarshalling body: %v\n", err)
+		log.Printf(msg)
+		w.WriteHeader(400)
+		e := ErrResponse{
+			HTTPStatusCode: 400,
+			Message:        msg,
+		}
+		json.NewEncoder(w).Encode(e)
+		return
+	}
+
+	a.Worker.AddTask(te.Sail)
+	log.Printf("Added sail %v\n", te.Sail.ID)
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(te.Sail)
+}
+
+func (a *Api) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(a.Worker.GetTasks())
+}
+
+func (a *Api) StopTaskHandler(w http.ResponseWriter, r *http.Request) {
+	taskID := chi.URLParam(r, "taskID")
+	if taskID == "" {
+		log.Printf("No taskID passed in request.\n")
+		w.WriteHeader(400)
+	}
+
+	tID, _ := uuid.Parse(taskID)
+	_, ok := a.Worker.Db[tID]
+	if !ok {
+		log.Printf("No task with ID %v found", tID)
+		w.WriteHeader(404)
+	}
+
+	taskToStop := a.Worker.Db[tID]
+
+	taskCopy := taskToStop
+	taskCopy.State = sail.Completed
+	a.Worker.AddTask(taskCopy)
+
+	log.Printf("Added task %v to stop container %v\n", taskToStop.ID, taskToStop.ContainerID)
+	w.WriteHeader(204)
+}
